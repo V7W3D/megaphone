166ca029296a1d6e8a3f474a51ccbfb2bdc07356
yahmiy <yanis.yahmi2002@gmail.com>
2023-05-19 15:05:20 +0200

    Thread notification

--- fil.c
+++ fil.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include "fil.h"
+#include "user.h"
 
 #define LEN_FILE 33554432 // 32 Mo
 #define LEN_MSG 255 
@@ -21,103 +23,141 @@ Chaque fil de discussions est constitué de:
 >Taille d'un fichiers < 33.5 Mo
 */
 
-int add_new_fil(struct fil *fils, char *adresse, int num_fil){
-    struct fil *new_fil = malloc(sizeof(struct fil));
+int fil_existant(fil *fils, int num_fil){
+    fil *tmp = fils;
+    while (tmp != NULL)
+    {
+        if (tmp->numero == num_fil)
+        {
+            return 1;
+        }
+        tmp = tmp->suivant;
+    }
+    return 0;
+}
+
+void add_new_fil(fil *fils, char *adresse, int num_fil){
+    if(fil_existant(fils, num_fil) == 1){
+        printf("Le fil %d existe déjà\n", num_fil);
+        return;
+    }
+    fil *new_fil = malloc(sizeof(fil));
     new_fil->numero = num_fil;
     new_fil->adresse = adresse;
     new_fil->billet = NULL;
     new_fil->fichier = NULL;
     new_fil->abonnes = NULL;
-    new_fil->gauche = NULL;
-    new_fil->droite = NULL;
+    new_fil->suivant = NULL;
 
-    struct fil *tmp = fils;
-    while (tmp != NULL)
-    {
-        if (tmp->numero > num_fil)
-        {
-            if (tmp->gauche == NULL)
-            {
-                tmp->gauche = new_fil;
-                return;
-            }
-            tmp = tmp->gauche;
-        }
-        else
-        {
-            if (tmp->droite == NULL)
-            {
-                tmp->droite = new_fil;
-                return;
-            }
-            tmp = tmp->droite;
-        }
-    }
-    
-}
-
-// Inserer une nouvelle personne dans la liste personne
-int add_new_personne(struct user *personnes, user *personne){
-    struct user *new_personne = malloc(sizeof(struct user));
-    new_personne->abonne = personne;
-    new_personne->suivant = NULL;
-
-    struct user *tmp = personnes;
+    fil *tmp = fils;
     while (tmp != NULL)
     {
         if (tmp->suivant == NULL)
         {
-            tmp->suivant = new_personne;
+            tmp->suivant = new_fil;
             return;
+        }
+        tmp = tmp->suivant;
+    }
+}
+
+
+// Inserer une nouvelle personne dans la liste personne d'un fil
+void add_new_user(fil *fils, int num_fil, user user){
+    fil *tmp = fils;
+    while (tmp != NULL)
+    {
+        if (tmp->numero == num_fil)
+        {
+            tmp->abonnes = add_user(tmp->abonnes, &user);
         }
         tmp = tmp->suivant;
     }
 }
 
 // Inserer un nouveau billet dans la liste billet
-int add_new_billet(struct billet *billets, char *message){
-    struct billet *new_billet = malloc(sizeof(struct billet));
+void add_new_billet(fil *fils, int num_fil, int id_proprietaire, char *message){
+    billet *new_billet = malloc(sizeof(billet));
+    new_billet->id_proprietaire = id_proprietaire;
     new_billet->message = message;
     new_billet->suivant = NULL;
 
-    struct billet *tmp = billets;
+    fil *tmp = fils;
     while (tmp != NULL)
     {
-        if (tmp->suivant == NULL)
+        if (tmp->numero == num_fil)
         {
-            tmp->suivant = new_billet;
-            return;
+            billet *tmp_billet = tmp->billet;
+            while (tmp_billet != NULL)
+            {
+                if (tmp_billet->suivant == NULL)
+                {
+                    tmp_billet->suivant = new_billet;
+                    return;
+                }
+                tmp_billet = tmp_billet->suivant;
+            }
         }
         tmp = tmp->suivant;
     }
 }
 
 // Inserer un nouveau fichier dans la liste fichier
-int add_new_fichier(struct fichier *fichiers, char *nom_fichier){
-    struct fichier *new_fichier = malloc(sizeof(struct fichier));
-    new_fichier->nom_fichier = nom_fichier;
+void add_new_fichier(fil *fils, int num_fil, int id_proprietaire, char *nom, char *data){
+    fichier *new_fichier = malloc(sizeof(fichier));
+    new_fichier->id_proprietaire = id_proprietaire;
+    new_fichier->nom = nom;
+    new_fichier->data = data;
     new_fichier->suivant = NULL;
 
-    struct fichier *tmp = fichiers;
+    fil *tmp = fils;
     while (tmp != NULL)
     {
-        if (tmp->suivant == NULL)
+        if (tmp->numero == num_fil)
         {
-            tmp->suivant = new_fichier;
-            return;
+            fichier *tmp_fichier = tmp->fichier;
+            while (tmp_fichier != NULL)
+            {
+                if (tmp_fichier->suivant == NULL)
+                {
+                    tmp_fichier->suivant = new_fichier;
+                    return;
+                }
+                tmp_fichier = tmp_fichier->suivant;
+            }
         }
         tmp = tmp->suivant;
+    }
+}
+
+void* notificationThread(void* arg) {
+    // Boucle pour gérer les notifications en continu
+    while (1) {
+        // Logique de gestion des notifications
+        printf("Traitement des notifications...\n");
+
+        // Temporisation de 20 secondes
+        sleep(20);
     }
 }
 
 
 
 
-
-
-/*
 int main(int argc, char const *argv[]){
+    pthread_t thread;
+    if (pthread_create(&thread, NULL, notificationThread, NULL) != 0) {
+        perror("Erreur lors de la création du thread de gestion des notifications");
+        exit(EXIT_FAILURE);
+    }
+
+    // Continuer avec les autres actions du serveur
+    printf("Serveur en cours d'exécution...\n");
+
+    // Attendre la fin du thread de gestion des notifications (ceci ne sera jamais atteint dans cet exemple)
+    pthread_join(thread, NULL);
 
     return 0;
 }
-*/
+
+
--- fil.h
+++ fil.h
@@ -1,49 +1,36 @@
 #ifndef FIL_H
 #define FIL_H
 
-struct user
-{
-    /* data */
-};
 
-
-
-struct billet
-{
+typedef struct billet{
     int numero; // numéro du billet
     int id_proprietaire; // id de l'auteur
     char* message; // message du billet < 255
     struct billet *suivant; // pointeur vers le billet suivant
-};
+} billet;
 
 
-struct fichier
-{
+typedef struct fichier {
     int numero; // numéro du fichier
     int id_proprietaire; // id de l'auteur
-    char nom[50]; // nom du fichier
+    char* nom; // nom du fichier
     char* data; // données du fichier [33.5 Mo]
     struct fichier *suivant; // pointeur vers le fichier suivant
-};
+} fichier;
 
 
-struct fil
-{
+typedef struct fil{
     int numero; // numéro du fil
     char *adresse; // adresse de multidifusion?
-    struct billet *billet; // (Pile) liste des billets difusés
-    struct fichier *fichier; // (Pile) liste des fichiers difusés
-    struct user *abonnes; // liste des personnes abonnées
-    struct fil *gauche; // pointeur vers le fil suivant
-    struct fil *droite; // pointeur vers le fil suivant
+    billet *billet; // (Pile) liste des billets difusés
+    fichier *fichier; // (Pile) liste des fichiers difusés
+    luser abonnes; // liste des personnes abonnées
+    struct fil *suivant; // pointeur vers le fil suivant
+} fil;
 
-};
-
-int add_new_fil(struct fil *fils, char *nom, char *adresse, int num_fil);
-int add_new_billet(struct fil *fils, int num_fil, int id_proprietaire, char *message);
-int add_new_fichier(struct fil *fils, int num_fil, int id_proprietaire, char *nom, char *data);
-int add_new_user(struct fil *fils, int num_fil, int id_user);
-
-
+void add_new_fil(fil *fils, char *adresse, int num_fil);
+void add_new_user(fil *fils, int num_fil, int id_user);
+void add_new_billet(fil *fils, int num_fil, int id_proprietaire, char *message);
+void add_new_fichier(fil *fils, int num_fil, int id_proprietaire, char *nom, char *data);
 
 #endif
